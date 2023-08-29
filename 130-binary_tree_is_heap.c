9ed52d17c56ff74f9eb3695d6dba70685e19ab9e
janviave tharwat <91113365+JanviaveTharwat@users.noreply.github.com>
2023-08-29 21:26:52 +0200

    tree

--- 130-binary_tree_is_heap.c
+++ 130-binary_tree_is_heap.c
@@ -3,7 +3,7 @@
 /**
  * binary_tree_size - measures the size of a binary tree
  * @tree: tree root
- * Return: size of the tree or 0 
+ * Return: size of the tree or 0
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
