466af258321c6720848da041782c15b788408030
janviave tharwat <91113365+JanviaveTharwat@users.noreply.github.com>
2023-08-30 18:59:18 +0200

    tree

--- 132-array_to_heap.c
+++ 132-array_to_heap.c
@@ -4,7 +4,7 @@
  * array_to_heap - builds a Max Binary Heap tree from an array
  * @array: input array
  * @size: size of array
- * Return: pointer to the root node of the created Binary Heap, or NULL
+ * Return: pointer to the root node of the created Binary Heap or NULL
  */
 heap_t *array_to_heap(int *array, size_t size)
 {
