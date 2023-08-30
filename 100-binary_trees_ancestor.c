0b8558fae6eefa39c27be98437e3952890a46637
amalsayedm <amal.sayedm2@gmail.com>
2023-08-30 20:43:01 +0200

    amal

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,55 +1,26 @@
 #include "binary_trees.h"
-binary_tree_t *recurse_ancestor(binary_tree_t *root,
-	const binary_tree_t *first, const binary_tree_t *second);
+
 /**
- * binary_trees_ancestor - find lowest common ancestor
- * @first: first node to find its ancestors
- * @second: second node to find its ancestors
+ * binary_trees_ancestor - Finds the lowest common ancestor of two nodes
+ * @first: the first node.
+ * @second: the second node.
  *
- * Return: lowest common ancestor Otherwise NULL
+ * Return: NULL, else return common ancestor.
  */
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
-	const binary_tree_t *second)
+		const binary_tree_t *second)
 {
-	binary_tree_t *root_node = NULL;
+	binary_tree_t *left, *right;
 
 	if (!first || !second)
 		return (NULL);
+	if (first == second)
+		return ((binary_tree_t *)first);
 
-	/* find root node */
-	root_node = (binary_tree_t *)first;
-	while (root_node->parent != NULL)
-		root_node = root_node->parent;
-
-	/* perform lowest common anscestor detection */
-	return (recurse_ancestor(root_node, first, second));
-}
-/**
- * recurse_ancestor - recursive to find lowest ancestor
- * @root: pointer to root
- * @first: pointer to one child
- * @second: pointer to other child
- *
- * Return: pointer to lower ancestor
- */
-binary_tree_t *recurse_ancestor(binary_tree_t *root,
-	const binary_tree_t *first, const binary_tree_t *second)
-{
-	binary_tree_t *left_ancestor = NULL, *right_ancestor = NULL;
-
-	if (root == NULL || first == NULL || second == NULL)
-		return (NULL);
-
-	if (root == first || root == second)
-		return (root);
-
-	left_ancestor = recurse_ancestor(root->left, first, second);
-	right_ancestor = recurse_ancestor(root->right, first, second);
-
-	if (left_ancestor && right_ancestor)
-		return (root);
-
-	if (left_ancestor != NULL)
-		return (left_ancestor);
-	return (right_ancestor);
+	left = first->parent, right = second->parent;
+	if (first == right || !left || (!left->parent && right))
+		return (binary_trees_ancestor(first, right));
+	else if (left == second || !right || (!right->parent && left))
+		return (binary_trees_ancestor(left, second));
+	return (binary_trees_ancestor(left, right));
 }
